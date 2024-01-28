0a6e69b31737355e695f8e32db3355dd46f2f2e5
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-28 17:35:51 +0000

    RSA Factoring Challenge

--- factors.h
+++ factors.h
@@ -1,9 +1,14 @@
-#ifndef FACTORS_H
-#define FACTORS_H
+#ifndef _FACTOR_H_
+#define _FACTOR_H_
 
-#include <vector>
 
-std::vector<std::pair<int, int>> factorize(int number);
-void factorize_file(const std::string& file_path);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+
+
+/* size_t getline(char **lineptr, size_t *n, FILE *stream); */
+int factorize(char *buffer);
 
 #endif
